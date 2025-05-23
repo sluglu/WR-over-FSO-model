25186a614931cd17b4c03aae6e75f94608979d80
Sluglu <eliot@bastoul.com>
2025-05-23 17:09:42 -0400

    adding sim stepping in clock test and adding a way to easily apply SyncE every tick

--- clock_model/slave_clock.m
+++ clock_model/slave_clock.m
@@ -1,17 +1,33 @@
 classdef slave_clock < wrclock
+    properties
+        tick_callback = [];  % Function handle or empty
+    end
     methods
         function obj = slave_clock(nom_freq, drift_ppb, jitter_std)
             obj@wrclock(nom_freq, drift_ppb, jitter_std);
         end
 
-        function syntonize(obj, new_freq)
-            % Override frequency as if synchronized via SyncE
-            obj.frequency = new_freq;
+        function tick(obj, sim_dt)
+
+            % Apply drift
+            obj.frequency = obj.nominal_freq * (1 + obj.drift_ppb * 1e-9);
+
+            % Execute optional callback function (for SyncE correction)
+            if ~isempty(obj.tick_callback)
+                obj.tick_callback(obj);  % Pass self (slave) into the callback
+            end
+            % Advance the clock time by 1 tick
+            obj.cycle_count = obj.cycle_count + obj.frequency * sim_dt;
         end
 
-        function apply_offset(obj, offset_s)
+        % function apply_freq_correction(obj, correction)
+        %     % Override frequency as if synchronized via SyncE
+        %     obj.frequency = obj.frequency + correction;
+        % end
+
+        function apply_offset_correction(obj, offset_s)
             % Convert offset in seconds to cycles and directly apply it
-            cycle_shift = offset_s * obj.nominal_freq;
+            cycle_shift = offset_s * obj.frequency;
             obj.cycle_count = obj.cycle_count + cycle_shift;
         end
     end
